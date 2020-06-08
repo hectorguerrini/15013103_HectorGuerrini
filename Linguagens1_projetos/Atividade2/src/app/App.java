4d849ed34d45f03d2f23175460bb14b6e3067b14
shpaisman <andre.shpaisman@hotmail.com>
2020-06-08 11:16:02 -0300

    main corrigida

--- Linguagens1_projetos/Atividade2/src/app/App.java
+++ Linguagens1_projetos/Atividade2/src/app/App.java
@@ -3,9 +3,17 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Bem Vindo\nHorário de trabalho atual: XXXX\nMENU DE OPÇÕES:\nPressione:");
-        System.out.println("0- Cadastrar novo membro");
-        System.out.println("1- Verificar horário de atuação");
-        System.out.println("2- Trocar horário de atuação");
-        System.out.println("3- Postar mensagem");
+        System.out.println("1- Cadastrar novo membro");
+        System.out.println("2- Apresentação dos membros cadastrados");
+        System.out.println("3- Excluir um membro");
+        System.out.println("4- Trocar jornada de trabalho");
+        System.out.println("5- Postar mensagem");
+        System.out.println("0- Sair do sistema");
     }
 }
+//Cadastrar novo membro
+//apresentacao dos membros cadastrados
+//Excluir um membro
+//trocar jornada de trabalho
+//postar msg
+//Sair do sistema
