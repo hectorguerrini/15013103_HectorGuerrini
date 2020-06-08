c20587de3025c99db8041590804e49e5a71246ee
HectorGuerrini <hectorumaga@hotmail.com>
2020-06-08 11:27:23 -0300

    menu

--- Linguagens1_projetos/Atividade2/src/app/App.java
+++ Linguagens1_projetos/Atividade2/src/app/App.java
@@ -1,17 +1,43 @@
 package app;
 
+import java.util.Scanner;
+
+import app.enums.Horarios;
+import app.models.MaskSociety;
+import app.models.Membro;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bem Vindo\nHorário de trabalho atual: XXXX\nMENU DE OPÇÕES:\nPressione:");
-        System.out.println("1- Cadastrar novo membro");
-        System.out.println("2- Apresentação dos membros cadastrados");
-        System.out.println("3- Excluir um membro");
-        System.out.println("4- Trocar jornada de trabalho");
-        System.out.println("5- Postar mensagem");
-        System.out.println("0- Sair do sistema");
-        System.out.println("Jornada de trabalho atual: ");
+        Scanner sc = new Scanner(System.in);
+        MaskSociety society = new MaskSociety(Horarios.horaNormal);
+        while(true){
+            System.out.println("Bem Vindo\nMENU DE OPÇÕES:\nPressione:");
+            System.out.println("1- Cadastrar novo membro");
+            System.out.println("2- Apresentação dos membros cadastrados");
+            System.out.println("3- Excluir um membro");
+            System.out.println("4- Trocar jornada de trabalho");
+            System.out.println("5- Postar mensagem");
+            System.out.println("0- Sair do sistema");
+            System.out.println("Jornada de trabalho atual: " + society.getHorarioTrabalho());
+            int menu = sc.nextInt();
+
+            switch (menu) {
+                case 1:
+
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    }
+    public static Membro menuCadastroMembro() {
+        return null;
+        
     }
 }
+
 //Cadastrar novo membro
 //apresentacao dos membros cadastrados
 //Excluir um membro
