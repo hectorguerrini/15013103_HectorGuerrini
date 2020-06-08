9b8e9af3d14bda47e5dfaad9a3588813dce54b18
shpaisman <andre.shpaisman@hotmail.com>
2020-06-08 11:12:27 -0300

    ss

--- Linguagens1_projetos/Atividade2/src/app/App.java
+++ Linguagens1_projetos/Atividade2/src/app/App.java
@@ -1,14 +1,11 @@
 package app;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
-        System.out.println(Funcoes.BigBrothers); 
-        
-    
-=======
-        
->>>>>>> 6ab59d547163a3b270324d515303b0229cb472e2
+        System.out.println("Bem Vindo\nHorário de trabalho atual: XXXX\nMENU DE OPÇÕES:\nPressione:");
+        System.out.println("0- Cadastrar novo membro");
+        System.out.println("1- Verificar horário de atuação");
+        System.out.println("2- Trocar horário de atuação");
+        System.out.println("3- Postar mensagem");
     }
 }
