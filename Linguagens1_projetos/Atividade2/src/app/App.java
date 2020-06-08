6208bd9112c9b9a1f442b6eee6275208d36c79ee
shpaisman <andre.shpaisman@hotmail.com>
2020-06-08 10:48:50 -0300

    discart

--- Linguagens1_projetos/Atividade2/src/app/App.java
+++ Linguagens1_projetos/Atividade2/src/app/App.java
@@ -4,5 +4,7 @@ import app.enums.Funcoes;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(Funcoes.BigBrothers); 
+        
+    
     }
 }
