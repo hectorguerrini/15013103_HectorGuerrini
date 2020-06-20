10c414d60ec9f529c63098af3d8f350d13b4e560
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2020-06-20 20:00:33 -0300

    começando sistema de vendas

--- Linguagens1_projetos/AtividadeP1/src/br/goHorse/sistemas/App.java
+++ Linguagens1_projetos/AtividadeP1/src/br/goHorse/sistemas/App.java
@@ -2,10 +2,13 @@ package br.goHorse.sistemas;
 
 import java.util.Scanner;
 
+import br.goHorse.sistemas.controllers.Venda;
+
 public class App {
 	final static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
 		int menu;
+		Venda sistemaVendas = new Venda();
 		do {
 			System.out.println("Pizzaria o Rato que Ri!");
 			System.out.println("1 - Nova Venda");
@@ -15,7 +18,9 @@ public class App {
 			menu = sc.nextInt();
 			switch (menu) {
 				case 1:
-					
+					System.out.println("Digite a descrição do pedido: ");
+					String inDesc = sc.next();					
+					sistemaVendas.novaVenda();
 					break;
 			
 				default:
---
+++ Linguagens1_projetos/AtividadeP1/src/br/goHorse/sistemas/controllers/Venda.java
@@ -0,0 +1,17 @@
+package br.goHorse.sistemas.controllers;
+
+import java.util.ArrayList;
+
+import br.goHorse.sistemas.models.Pedido;
+
+public class Venda {
+	private ArrayList<Pedido> listaPedidos = new ArrayList<Pedido>();
+	
+	public Venda() {
+	}
+
+	public void novaVenda(){
+
+	}
+
+}
