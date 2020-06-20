628114c8e8fa5408c85cc023f3bed3241be368e6
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2020-06-20 19:20:06 -0300

    estrutura projeto e enums

---
+++ Linguagens1_projetos/AtividadeP1/src/br/goHorse/sistemas/enums/EstadoPedido.java
@@ -0,0 +1,5 @@
+package br.goHorse.sistemas.enums;
+
+public enum EstadoPedido {
+	REALIZADO, PREPARACAO, SAIU_PARA_ENTREGA, ENTREGUE, DEVOLVIDO;
+}
---
+++ Linguagens1_projetos/AtividadeP1/src/br/goHorse/sistemas/enums/FormaPagamento.java
@@ -0,0 +1,5 @@
+package br.goHorse.sistemas.enums;
+
+public enum FormaPagamento {
+	DINHEIRO, DEBITO, CREDITO, VALE_ALIMENTACAO, VALE_REFEICAO;
+}
