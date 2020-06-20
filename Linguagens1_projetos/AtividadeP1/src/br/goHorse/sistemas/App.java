e68cf840cbfc0b830be4b379a934bcd9a21d1214
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2020-06-20 19:26:22 -0300

    menu sysout pronto

--- Linguagens1_projetos/AtividadeP1/src/br/goHorse/sistemas/App.java
+++ Linguagens1_projetos/AtividadeP1/src/br/goHorse/sistemas/App.java
@@ -1,6 +1,27 @@
 package br.goHorse.sistemas;
+
+import java.util.Scanner;
+
 public class App {
+	final static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Pizzaria o Rato que Ri!");
+		int menu;
+		do {
+			System.out.println("Pizzaria o Rato que Ri!");
+			System.out.println("1 - Nova Venda");
+			System.out.println("2 - Verificar pedidos");
+			System.out.println("3 - Alterar Pedidos");
+			System.out.println("0 - Sair");
+			menu = sc.nextInt();
+			switch (menu) {
+				case 1:
+					
+					break;
+			
+				default:
+					break;
+			}
+		} while (menu != 0);
+		
     }
 }
