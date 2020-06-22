333967a373bb32db6ec4ea9dd64c7800e96bff89
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2020-06-22 17:56:17 -0300

    classe de autenticação

---
+++ Linguagens1_projetos/AtividadeP1/src/br/goHorse/sistemas/controllers/Authenticate.java
@@ -0,0 +1,29 @@
+package br.goHorse.sistemas.controllers;
+
+import java.util.Scanner;
+
+import br.goHorse.sistemas.interfaces.Auth;
+import br.goHorse.sistemas.models.Usuario;
+
+public class Authenticate implements Auth {
+
+	public boolean confirmacaoUsuario(Usuario user){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Validação de Usuario.");
+		System.out.println("Digite sua senha para continuar: ");
+		String senha = sc.next();
+		boolean valido = this.validarSenha(user, senha);
+		sc.close();
+		return valido;
+	}
+	@Override
+	public boolean validarSenha(Usuario user, String senha) {
+		if(user.getSenha().equals(senha)){
+			System.out.println("Valido");
+			return true;
+		}
+		System.out.println("Senha Invalida!");
+		return false;
+	}
+	
+}
